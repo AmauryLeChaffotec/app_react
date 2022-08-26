c304afd7418ba821ffa91bf90cc0c06bd8dd40c1
Hoali_ALC <amaury.lechaffotec@gmail.com>
2022-08-26 14:53:58 +0200

    loader delay

--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -4,6 +4,7 @@ import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
 import PokemonService from '../services/pokemon-service'
 import {Link} from 'react-router-dom';
+import PokemonSearch from '../components/pokemon-search';
   
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
@@ -22,6 +23,7 @@ const PokemonList: FunctionComponent = () => {
       <h1 className="center">Pokédex</h1>
       <div className="container"> 
         <div className="row"> 
+        <PokemonSearch />
         {pokemons.map(pokemon => (
           <PokemonCard key={pokemon.id} pokemon={pokemon}/>
         ))}
--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -52,6 +52,12 @@ export default class PokemonService {
         .catch(error => this.handleError(error))
     }
 
+    static searchPokemon(term: string ): Promise<Pokemon[]>{
+        return fetch(`http://localhost:3001/pokemons?q=${term}`)
+        .then(response => response.json())
+        .catch(error => this.handleError(error));
+    }
+
     static handleError(error: Error): void {
 
         console.error(error)
